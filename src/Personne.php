cf68040abf46f4d44c7187f263f2a0e4e96c2049
Stanis Humez <stanis@netvolution.fr>
2024-12-10 16:45:11 +0100

    Correction PHPStan et PHPUnit

--- src/Personne.php
+++ src/Personne.php
@@ -1,13 +1,16 @@
 <?php
+namespace App;
+
+use DateTime;
 
 class Personne {
     // Attributs
-    private $nom;
-    private $prenom;
-    private $dateNaissance;
-    private $taille; // en cm
-    private $poids; // en kg
-    private $cheveux;
+    private string $nom;
+    private string $prenom;
+    private DateTime $dateNaissance;
+    private int $taille; // en cm
+    private int $poids; // en kg
+    private string $cheveux;
 
     // Constructeur
     public function __construct($nom, $prenom, $dateNaissance, $taille, $poids, $cheveux) {
@@ -20,18 +23,20 @@ class Personne {
     }
 
     // Méthode pour calculer l'IMC
-    public function imc() {
-        // Conversion de la taille de cm à m
+    public function imc(): float|int
+    {
+        // Conversion de la taille de cm à mètres
         $tailleEnMetres = $this->taille / 100;
         // Calcul de l'IMC : poids / (taille en mètres)^2
         return $this->poids / ($tailleEnMetres * $tailleEnMetres);
     }
 
     // Méthode pour afficher les informations de la personne
-    public function afficherInformations() {
+    public function afficherInformations(): void
+    {
         echo "Nom : " . $this->nom . "\n";
         echo "Prénom : " . $this->prenom . "\n";
-        echo "Date de naissance : " . $this->dateNaissance . "\n";
+        echo "Date de naissance : " . $this->dateNaissance->format("Y-m-d") . "\n";
         echo "Taille : " . $this->taille . " cm\n";
         echo "Poids : " . $this->poids . " kg\n";
         echo "Couleur de cheveux : " . $this->cheveux . "\n";
--- tests/PersonneTest.php
+++ tests/PersonneTest.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Personne;
 use PHPUnit\Framework\TestCase;
 
 class PersonneTest extends TestCase {
     public function testImc() {
-        $personne = new Personne("Doe", "John", "01/01/2000", 180, 80, "brun");
+        $personne = new Personne("Doe", "John", new DateTime("2000-01-01"), 180, 80, "brun");
         $this->assertEqualsWithDelta(24.69, $personne->imc(), 0.01, "L'IMC de John Doe est incorrect");
     }
 }
